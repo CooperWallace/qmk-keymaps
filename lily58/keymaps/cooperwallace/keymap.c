3236c13b96fc2676b8584a370867df45f47ec9e8
Cooper Wallace <CooperWallace@users.noreply.github.com>
2026-02-28 12:21:33 -0700

    Bind RAISE numbers to numpad

--- lily58/keymaps/cooperwallace/keymap.c
+++ lily58/keymaps/cooperwallace/keymap.c
@@ -120,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   TG(_GAME), TG(_GAME2), _______, _______, _______, _______,                     _______, KC_INS, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_PGUP, KC_HOME,
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_END,
+  KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                     KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_PGUP, KC_HOME,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_END,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                              _______, _______, KC_MPLY,  _______, _______,  _______, _______, _______
 ),
